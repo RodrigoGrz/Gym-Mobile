e177ce942f0f94d4a10f97da383c1e6b68a81f29
Rodrigo <rodrigo.gregorio1@hotmail.com>
2023-07-03 21:57:07 -0300

    feat: HTTP Client

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,11 +1,14 @@
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
+import { api } from '@services/api';
+
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+import { AppError } from '@utils/AppError';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
@@ -25,6 +28,8 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+    const toast = useToast();
+
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
     });
@@ -34,8 +39,23 @@ export function SignUp() {
         navigation.goBack();
     }
 
-    function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-        console.log([name, email, password, password_confirm])
+    async function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+        try {
+            const response = await api.post('/users', {
+                name,
+                email,
+                password,
+            });
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.'
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
     }   
 
     return (
